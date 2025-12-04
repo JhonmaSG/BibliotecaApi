8d2349ec51a18d859fe749cd0a3561cec627fe11
JhonmaSG <jserrano@udistrital.edu.co>
2025-12-03 20:51:50 -0500

    Created class Author and modify Book Models

---
+++ BibliotecaApi/Models/Author.cs
@@ -0,0 +1,14 @@
+﻿namespace BibliotecaApi.Models
+{
+    public class Author
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public DateTime BirthDate { get; set; }
+        public string Nationality { get; set; } = null!;
+
+        // Navegacion
+        public List<Book> Books { get; set; } = new();
+    }
+}
--- BibliotecaApi/Models/Book.cs
+++ BibliotecaApi/Models/Book.cs
@@ -3,11 +3,19 @@
     public class Book
     {
         public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string ISBN { get; set; }
-        public string YearPublication { get; set; }
-        public string AuthorId { get; set; }
+        public string Title { get; set; } = null!;
+        public string ISBN { get; set; } = null!;
+        public DateTime YearPublication { get; set; }
+        public string Genero { get; set; } = null!;
+        public int numberPages { get; set; }
         public string Stock {  get; set; }
 
+        // Para Borrado lógico
+        public bool EstaActivo { get; set; } = true;
+        public DateTime fechaCreacion { get; set; } = DateTime.Now;
+
+        // Claves foráneas y navegación
+        public int AuthorForeignKey { get; set; }
+        public required Author Author { get; set; } = null!;
     }
 }
