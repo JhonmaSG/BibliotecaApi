000d5abf62445363add80375c4e88b394827f071
jhonm <jhonm@JHON>
2025-12-03 19:53:14 -0500

    Add project files.

---
+++ BibliotecaApi/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BibliotecaApi.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public string YearPublication { get; set; }
+        public string AuthorId { get; set; }
+        public string Stock {  get; set; }
+
+    }
+}
